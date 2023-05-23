b1aeff8c12f1c5d2f9f8820d5b0bceb3d9cab7bc
Clarice Lopes <claricelopespb@gmail.com>
2023-05-23 17:49:10 -0300

    material da semana 4

--- semana4/E2semana4.py
+++ semana4/E2semana4.py
@@ -1,1 +1,24 @@
+from random import randint
+from time import sleep
 
+
+def sorteia(lista): 
+    print('sorteando 5 valores da lista: ') 
+    for cont in range(0, 5): 
+        n = randint(1, 10)
+        lista.append(n)
+        print(f'{n} ', end='', flush=True)
+        sleep(0.3)
+    print('printo')
+
+
+def somaPar(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'soamando os valores pares da {lista}, temos: {soma} ') 
+
+numeros=list()
+sorteia=(numeros)  
+somaPar=(numeros) 
