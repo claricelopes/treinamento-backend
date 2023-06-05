5b7f74a8037e79ddaa1518fdb6d6d25a017c8d90
Clarice Lopes <claricelopespb@gmail.com>
2023-06-05 16:52:54 -0300

    material semana4

--- semana4/E4semana4.py
+++ semana4/E4semana4.py
@@ -1,1 +1,18 @@
+def notas(*n, sit=False):
+    r = dict()
+    r['total'] = len(n) 
+    r['maior'] = max(n)
+    r['menor'] = min(n) 
+    r['média'] = sum(n)/len(n)
+    if sit:
+        if r['média'] >= 7:
+            r['situação'] = 'boa'
+        elif r['média'] >= 5:
+            r['situação'] = 'razoável'
+        else:
+            r['situação'] = 'ruim'
+    return r 
 
+
+resp = notas(8.5, 6.0, 3.8, sit=True)   
+print(resp) 
